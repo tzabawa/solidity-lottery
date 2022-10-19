1c362c313e2e08119b01d494576d639a9dc4c115
Tim Zabawa <timothyzabawa@gmail.com>
2022-10-19 10:34:07 -0400

    First commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifiers; MIT
+
+pragma solidity ^0.8.9;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function pickWinner() public restrictedToManager {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    modifier restrictedToManager() {
+        require(msg.sender == manager);
+        _;
+    }
+}
